d26367cb9152d16fe6a20f77ff8a610bab0ffc98
Neel Shah <neelakansha85@Neels-MacBook-Pro.local>
2014-02-24 23:04:05 -0500

    Initial Commit

---
+++ NeeliOS1/MyScene.h
@@ -0,0 +1,13 @@
+//
+//  MyScene.h
+//  NeeliOS1
+//
+
+//  Copyright (c) 2014 Neel Shah. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface MyScene : SKScene
+
+@end
---
+++ NeeliOS1/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  NeeliOS1
+//
+
+//  Copyright (c) 2014 Neel Shah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface ViewController : UIViewController
+
+@end
